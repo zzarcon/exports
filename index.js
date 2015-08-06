c84e03150892fc4a612cf29feceeddba3e7a9dfd
Hector Leon Zarco Garcia <hector.zarco@king.com>
2015-08-06 16:13:23 +0200

    Always export to window

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ module.exports = function (options) {
   var module = options.module;
   var name = options.name; 
   var definition = options.definition;
-  
+
 	if (typeof module !== 'undefined' && module.exports) {
     // CommonJS
     module.exports = definition();
   } else if (typeof define === 'function' && define.amd) {
     // AMD
     define(definition);
-  } else if (typeof window != 'undefined') {
+  } 
+
+  if (typeof window != 'undefined') {
     // Global Variables
     window[name] = definition();
   }
