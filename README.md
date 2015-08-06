a98bcc85634b03e2dc4e01e29e82d41da3e9042b
Hector Leon Zarco Garcia <hector.zarco@king.com>
2015-08-06 15:59:19 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# exports
-Shortcut for exports your modules in CommonJS, AMD and Globals
+# Exports [![Build Status](https://travis-ci.org/zzarcon/exports.svg?branch=master)](https://travis-ci.org/zzarcon/exports)
+
+> Exports
+
+## Install
+
+```
+$ npm install --save exportable
+```
+
+
+## Usage
+
+```js
+TODO: Add examples
+
+```
+
+
+## License
+
+MIT © [zzarcon](http://hectorleonzarco.com)
---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function (options) {
+  var module = options.module;
+  var name = options.name; 
+  var definition = options.definition;
+  
+	if (typeof module !== 'undefined' && module.exports) {
+    // CommonJS
+    module.exports = definition();
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(definition);
+  } else if (typeof window != 'undefined') {
+    // Global Variables
+    window[name] = definition();
+  }
+};
